08033122bf73e203af0553157b074ffbf92f7a76
kimbyeolhee <qufgml0216@naver.com>
2023-09-21 07:54:44 +0000

    [Add] finetuning

--- finetune.py
+++ finetune.py
@@ -1,32 +1,90 @@
 import os
 import sys
 import argparse
+
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"  # Arrange GPU devices starting from 0
+os.environ["CUDA_VISIBLE_DEVICES"]= "0"
+
 from pathlib import Path
 from omegaconf import OmegaConf
-from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training, AutoPeftModelForCausalLM
+from peft import get_peft_model, prepare_model_for_kbit_training
 from utils.model_loader import load_model_and_tokenizer
 from dataset import get_dataset
 from utils.LoRA import find_all_linear_names
-from utils.setup import set_device, seed_everything
+from utils.setup import seed_everything
+from utils.create_config import create_peft_config
+import torch
+from transformers import TrainingArguments, Trainer, DataCollatorForLanguageModeling
+from utils.merge_weights import merge_weights
 
 wd = Path(__file__).parent.parent.resolve()
 sys.path.append(str(wd))
 
-def train(model, tokenizer, dataset, output_dir):
+
+
+def train(model, tokenizer, dataset, config):
     # Enabling gradient checkpointing to reduce memory usage for training
     model.gradient_checkpointing_enable()
 
     # using PEFT's prepare_model_for_kbit_training
     model = prepare_model_for_kbit_training(model)
 
-    # get LoRA module names
+    # get modules to apply LoRA
     modules = find_all_linear_names(model)
-    print("📌")
-    print(modules)
+
+    # create PEFT config
+    peft_config = create_peft_config(modules)
+    model = get_peft_model(model, peft_config)
+
+    # training arguments
+    training_args = TrainingArguments(
+        per_device_train_batch_size=2,
+        gradient_accumulation_steps=4,
+        warmup_steps=2,
+        max_steps=15,
+        output_dir=config.setup.output_dir,
+        # num_train_epochs=1,
+        learning_rate = 2e-4,
+        fp16=True,
+        optim = "paged_adamw_8bit", # 32bit
+    )
+
+    # train
+    trainer = Trainer(
+        model=model,
+        train_dataset=dataset,
+        args=training_args,
+        data_collator=DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False),
+    )
+    model.config.use_cache = False
+
+    train_result = trainer.train()
+    metrics = train_result.metrics
+    trainer.log_metrics("train", metrics)
+    trainer.save_metrics("train", metrics)
+    trainer.save_state()
+    print("-------- Training Done! ----------")
+    print(metrics)
+
+    # save model
+    save_path = os.path.join(config.setup.output_dir, config.model.name_or_path.split("/")[-1], config.setup.experiment_name, "8bit")
+    os.makedirs(save_path, exist_ok=True)
+    trainer.model.save_pretrained(save_path, safe_serialization=True) # json/encoder.py's def default needs to support arbirary iterators
+
+    # Reload model in FP16 and merge it with LoRA weights
+    merge_weights(model, tokenizer, config, save_path)
+
+    # delete memory cache
+    del model
+    del trainer
+    torch.cuda.empty_cache()
+
 
 def main(config, device):
     model, tokenizer = load_model_and_tokenizer(config.model.name_or_path)
     dataset = get_dataset(model, tokenizer, config.setup.seed)
+
+    train(model, tokenizer, dataset, config)
 
 
 
@@ -38,8 +96,12 @@ if __name__ == "__main__":
     config = OmegaConf.load(
         f"{wd}/configs/{args.config}.yaml"
     )
+ 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Device: ", device)
+    print("Current cuda device: ", torch.cuda.current_device())
+    print("Count of using GPUs: ", torch.cuda.device_count())
 
-    device = set_device(config.setup.device_number)
     seed_everything(config.setup.seed)
 
     main(config, device)
